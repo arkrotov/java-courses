602ed440bb868452273d70588a1f2be856d733da
Petr Arsentev <arsentyev.petr@otr.ru>
2015-04-29 22:09:09 +0300

    #19 jdbc
    (cherry picked from commit a3fd1e2)

--- clinic-pet-web/src/main/java/ru/parsentev/store/UserCache.java
+++ clinic-pet-web/src/main/java/ru/parsentev/store/UserCache.java
@@ -4,40 +4,59 @@ import ru.parsentev.models.User;
 
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * TODO: comment
  * @author parsentev
  * @since 18.04.2015
  */
-public class UserCache {
+public class UserCache implements Storage {
 	private static final UserCache INSTANCE = new UserCache();
 
-	private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<Integer, User>();
+	private final Storage storage = new MemoryStorage();
 
 	public static UserCache getInstance() {
 		return INSTANCE;
 	}
 
+	@Override
 	public Collection<User> values() {
-		return this.users.values();
+		return INSTANCE.values();
 	}
 
-	public void add(final User user) {
-		this.users.put(user.getId(), user);
+	@Override
+	public int add(final User user) {
+		return this.storage.add(user);
 	}
 
+	@Override
 	public void edit(final User user) {
-		this.users.replace(user.getId(), user);
+		this.storage.edit(user);
 	}
 
+	@Override
 	public void delete(final int id) {
-		this.users.remove(id);
+		this.storage.delete(id);
 	}
 
+	@Override
 	public User get(final int id) {
-		return this.users.get(id);
+		return this.storage.get(id);
 	}
 
+	@Override
+	public User findByLogin(final String login) {
+		return this.storage.findByLogin(login);
+	}
 
+	@Override
+	public int generateId() {
+		return this.storage.generateId();
+	}
+
+	@Override
+	public void close() {
+		this.storage.close();
+	}
 }
