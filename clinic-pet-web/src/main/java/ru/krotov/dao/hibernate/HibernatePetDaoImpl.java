3b7620a70a355e256c71022fa9bcf634d0e2921d
Artem Krotov <artkrotov7@gmail.com>
2017-10-04 23:25:31 +0300

    Added HibernateDaoImpl for next entities: Pet

--- clinic-pet-web/src/main/java/ru/krotov/dao/hibernate/HibernatePetDaoImpl.java
+++ clinic-pet-web/src/main/java/ru/krotov/dao/hibernate/HibernatePetDaoImpl.java
@@ -1,44 +1,111 @@
 package ru.krotov.dao.hibernate;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import ru.krotov.dao.PetDao;
 import ru.krotov.models.Pet;
-import ru.krotov.service.*;
+import ru.krotov.models.User;
 
 import java.util.Collection;
+import java.util.List;
 
-/**
- * Created by me on 19.08.17.
- */
-public class HibernatePetDaoImpl implements PetService {
+public class HibernatePetDaoImpl implements PetDao {
+    private final SessionFactory factory;
 
+    public HibernatePetDaoImpl() {
+        factory = new Configuration().configure().buildSessionFactory();
+    }
 
     @Override
     public Collection<Pet> values() {
-        return null;
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            return session.createQuery("from Pet").list();
+        } finally {
+            tx.commit();
+            session.close();
+        }
     }
 
     @Override
-    public int add(Pet user) {
-        return 0;
+    public int add(final Pet pet) {
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            session.save(pet);
+            return pet.getId();
+        } finally {
+            tx.commit();
+            session.close();
+        }
     }
 
     @Override
-    public void edit(Pet user) {
-
+    public void edit(Pet pet) {
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            session.update(pet);
+        } finally {
+            tx.commit();
+            session.close();
+        }
     }
 
     @Override
     public void delete(int id) {
-
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            session.delete(get(id));
+        } finally {
+            tx.commit();
+            session.close();
+        }
     }
 
     @Override
     public Pet get(int id) {
-        return null;
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            return (Pet) session.get(Pet.class, id);
+        } finally {
+            tx.commit();
+            session.close();
+        }
     }
 
     @Override
-    public Pet findByName(String login) {
-        return null;
+    public Pet findByName(String name) {
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            final Query query = session.createQuery("from Pet as pet where pet.name=:name");
+            query.setString("name", name);
+            return (Pet) query.iterate().next();
+        } finally {
+            tx.commit();
+            session.close();
+        }
+    }
+
+    @Override
+    public Collection<Pet> getByOwner(User user) {
+        final Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            final Query query = session.createQuery("from Pet as pet where pet.user.id=:userId");
+            query.setInteger("userId", user.getId());
+            return (List<Pet>) query.list();
+        } finally {
+            tx.commit();
+            session.close();
+        }
     }
 
     @Override
@@ -48,6 +115,6 @@ public class HibernatePetDaoImpl implements PetService {
 
     @Override
     public void close() {
-
+        this.factory.close();
     }
 }
